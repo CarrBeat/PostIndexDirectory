41570264c1c35c5ce5d586f5b6429f78783b2da5
CarrBeat <carrbeat@gmail.com>
2022-03-02 02:24:59 +0300

    second commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module carrbeat.postindexdirectory {
     requires javafx.controls;
     requires javafx.fxml;
-
-
     opens carrbeat.postindexdirectory to javafx.fxml;
     exports carrbeat.postindexdirectory;
 }
