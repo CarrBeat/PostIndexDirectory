a875ce73a1938eb8f33bb8d351db045aa413dce0
CarrBeat <carrbeat@gmail.com>
2022-03-04 15:00:24 +0300

    test ComboBoxAutoComplete

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module carrbeat.postindexdirectory {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.java;
+    requires java.desktop;
+    requires org.apache.commons.lang3;
     opens carrbeat.postindexdirectory to javafx.fxml;
     exports carrbeat.postindexdirectory;
 }
